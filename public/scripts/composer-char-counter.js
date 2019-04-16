39eea213e94fbc579c9f48311dc2dcb3ca29c297
Julian <julian@juliantomlindesign.com>
2019-04-16 23:18:32 +0000

    built the basic structure of the webpage and implimented the text field with character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+  $("textarea").on("keypress input propertychange paste cut change", function() {
+
+    let lettersLeft = (140 - $(this).val().length)
+    let counterObject = $(".counter").text((140 - $(this).val().length))
+    counterObject.text(lettersLeft)
+
+    if (lettersLeft < 0){
+      counterObject.css({color: 'red'})
+    } else {
+      counterObject.css({color: 'black'})
+
+    }
+
+  })
+})
