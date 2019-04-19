e760a6d7860908afb97b5f64f49568cc422070ab
Julian <julian@juliantomlindesign.com>
2019-04-19 22:12:44 +0000

    added login button and partially implimted login overlay

---
+++ public/scripts/login-button.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('button.login').click(function (){
+    $('button.logout').slideToggle("fast", function() {
+    })
+    $('.loggedin p').toggleClass('shown')
+    $('.loginArea').slideToggle('fast')
+  })
+})
--- public/scripts/toggle-focus.js
+++ public/scripts/toggle-focus.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('button.comp').click(function (){
-    console.log('button')
     $('section.new-tweet').slideToggle("fast", function (){     //shows the compose tweet area and puts the textarea in focus
       $('textarea').select()
       document.documentElement.scrollTop = 0;
